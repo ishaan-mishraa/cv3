0d7db252ef4a16608dda57a5276354fdb4258095
Ishaan Mishra <ishaancodes01@gmail.com>
2024-06-18 13:37:33 +0530

    deployable commit

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
 const app = express();
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, ()=>{
-    console.log(`Server started on port ${PORT}`)
+const PORT = process.env.PORT || 5000;
+
+// Proxy requests to the React development server
+app.use(
+  '/',
+  createProxyMiddleware({
+    target: 'http://localhost:5173',
+    changeOrigin: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Express server is running on port ${PORT}`);
 });
